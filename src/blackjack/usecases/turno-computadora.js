c218ec38fd72f74f4e0ca3d521017fac8082f02e
willt <luisgerardo_willy@outlook.com>
2023-01-31 17:40:56 -0600

    First commit

---
+++ src/blackjack/usecases/turno-computadora.js
@@ -0,0 +1,44 @@
+// turno de la computadora
+import { pedirCarta, valorCarta, crearCartaHTML } from './'
+
+/**
+ * 
+ * @param {Number} puntosMinimos puntos minimos que necesita la computadora para ganar
+ * @param {HTMLElement} puntosHTML elemento html para mostrar los puntos
+ * @param {HTMLElement} divCartasComputadora elemento html para mostrar los puntos
+ * @param {Array<String>} deck 
+ */
+export const turnoComputadora = (puntosMinimos, puntosHTML, divCartasComputadora, deck = []) => {
+
+    if (!puntosMinimos) throw new Error('Puntos minimos son necesrio');
+    if (!puntosHTML) throw new Error('Es necesario');
+
+    let puntosComputadora = 0;
+
+    do {
+        const carta = pedirCarta(deck);
+
+        puntosComputadora = puntosComputadora + valorCarta(carta);
+        puntosHTML.innerText = puntosComputadora;
+
+        const imgCarta = crearCartaHTML(carta);
+        divCartasComputadora.append(imgCarta);
+
+        if (puntosMinimos > 21) {
+            break;
+        }
+
+    } while ((puntosComputadora < puntosMinimos) && (puntosMinimos <= 21));
+
+    setTimeout(() => {
+        if (puntosComputadora === puntosMinimos) {
+            alert('Nadie gana :(');
+        } else if (puntosMinimos > 21) {
+            alert('Computadora gana')
+        } else if (puntosComputadora > 21) {
+            alert('Jugador Gana');
+        } else {
+            alert('Computadora Gana')
+        }
+    }, 100);
+}
